9b7212e835464be872caa47372f097c403706899
Sarah Gooneratne <sarah.gooneratne01@student.csulb.edu>
2025-12-01 17:36:45 -0800

    added majority circuit function module

---
+++ veriHashTakeTwo/veriHashTakeTwo.srcs/sources_1/new/majority.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/01/2025 04:21:39 PM
+// Design Name: 
+// Module Name: majority
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module majority(
+    input [31:0] A,
+    input [31:0] B,
+    input [31:0] C,
+    output Majority_Out
+    );
+    
+    assign Majority_Out = (A & B) | (A & C) | (B & C);
+    
+endmodule
