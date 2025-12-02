634e9b9d02a175ef494106d5e46e69a941a1673f
Sarah Gooneratne <sarah.gooneratne01@student.csulb.edu>
2025-12-01 18:05:14 -0800

    added new function for hash computing

---
+++ veriHashTakeTwo/veriHashTakeTwo.srcs/sources_1/new/ch.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/01/2025 05:55:58 PM
+// Design Name: 
+// Module Name: ch
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ch(
+    input E,
+    input F,
+    input G,
+    output Ch_Out
+    );
+endmodule
